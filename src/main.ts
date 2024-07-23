140ff86a959d8c19797bd82ae4aaa315d9ba96ab
PAM <pa.martin@outlook.fr>
2024-07-23 11:21:07 +0100

    Optimisations et nettoyage du code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import api from 'api';
-const developersPandascore = api('@developers-pandascore/v2#1lcfevlxltohmi');
 import dotenv from 'dotenv';
-import fs from 'fs';
+import fs from 'fs-extra';
 import * as ics from 'ics';
 import * as ftp from 'basic-ftp';
-import {v4 as uuidv4} from 'uuid';
 
+import Event from './models/Event';
 import Match from './models/Match';
 import Team from './models/Team';
 
@@ -13,6 +11,7 @@ dotenv.config();
 
 const TOKEN = process.env.PANDASCORE_TOKEN!;
 const FTP_HOST = process.env.FTP_HOST!;
+const FTP_PORT = process.env.FTP_PORT || '21';
 const FTP_USER = process.env.FTP_USER!;
 const FTP_PASS = process.env.FTP_PASS!;
 const FTP_PATH = process.env.FTP_PATH!;
@@ -26,67 +25,58 @@ const options = {
     }
 };
 
-developersPandascore.auth(TOKEN);
-
-let teams: Team[] = [];
-let upcomingMatches: Match[] = [];
-let pastMatches: Match[] = [];
-
-async function getTeams(teamName: string) {
-    await fetch(`${BASE_URL}/teams?search[name]=${teamName}`, options)
+async function getTeams(teamName: string): Promise<Team[]> {
+    return fetch(`${BASE_URL}/teams?search[name]=${teamName}`, options)
     .then(response => response.json())
     .then(data => {
-        teams = data;
-        console.log(`Found ${teams.length} teams`);
+        console.log(`Found ${data.length} teams`);
+        return data;
     })
     .catch(err => console.error(err));
 }
 
-async function getUpcomingMatches(teamId: number) {
-    await fetch(`${BASE_URL}/matches/upcoming?filter[opponent_id]=${teamId}`, options)
+async function getUpcomingMatches(team: Team): Promise<Match[]> {
+    return fetch(`${BASE_URL}/matches/upcoming?filter[opponent_id]=${team.id}`, options)
     .then(response => response.json())
     .then(data => {
-        upcomingMatches.push(...data);
-        console.log(`Found ${data.length} upcoming matches for team ${teams.find(t => t.id === teamId)?.slug}`);
+        console.log(`Found ${data.length} upcoming matches for team ${team.slug}`);
+        return data;
     })
     .catch(err => console.error(err));
 }
 
-async function getPastMatches(teamId: number, startDate: string, endDate: string) {
-    await fetch(`${BASE_URL}/matches/past?filter[opponent_id]=${teamId}&range[scheduled_at]=${startDate},${endDate}`, options)
+async function getPastMatches(team: Team, startDate: string, endDate: string): Promise<Match[]> {
+    return fetch(`${BASE_URL}/matches/past?filter[opponent_id]=${team.id}&range[scheduled_at]=${startDate},${endDate}`, options)
     .then(response => response.json())
     .then(data => {
-        pastMatches.push(...data);
-        console.log(`Found ${data.length} past matches for team ${teams.find(t => t.id === teamId)?.name}`);
+        console.log(`Found ${data.length} past matches for team ${team.slug}`);
+        return data;
     })
     .catch(err => console.error(err));
 }
 
-function updateEventSummary(event: any, match: Match) {
-    const result = match.results.map(r => `${r.team_id} ${r.score}`).join(' - ');
-    return `${event.SUMMARY}: ${result}`;
-}
-
-function createIcsEvent(match: Match) {
+function createIcsEvent(match: Match): Event {
     return {
-        uid: `${uuidv4()}@pa-martin.github.io`,
-        sequence: match.id,
-        created: new Date().toISOString(),
-        lastModified: new Date().toISOString(),
-        start: match.scheduled_at ?? match.begin_at,
-        end: match.end_at || match.scheduled_at + 3600000 * (match.match_type === 'best_of' ? 3 : 1),
-        title: match.status !== 'finished' ?
-            `[${match.league.name}] ${match.name}` :
-            `[${match.league.name}] ${match.name.replace('vs', match.results.map(r => r.score).join(' - '))}`,
+        uid: `${match.id}@pa-martin.github.io`,
+        sequence: 0,
+        created: Date.now(),
+        lastModified: Date.now(),
+        start: Date.parse(match.scheduled_at ?? match.begin_at),
+        end: match.end_at
+            ? Date.parse(match.end_at)
+            : Date.parse(match.scheduled_at) + 3600000 * match.number_of_games,
+        title: match.status !== 'finished'
+            ? `[${match.league.name}] ${match.name}`
+            : `[${match.league.name}] ${match.name.replace('vs', match.results.map(r => r.score).join(' - '))}`,
         url: match.streams_list[0]?.raw_url || '',
+        location: match.streams_list[0]?.raw_url || '',
     };
 }
 
-async function generateIcsFile(events: any[]) {
+async function generateIcsFile(events: Event[]): Promise<string | Error> {
     let { error, value } = ics.createEvents(events);
     if (error) {
-        console.error(error);
-        return;
+        return error;
     }
     const calendarSettings = [
         'PRODID:pa-martin.github.io/ics',
@@ -95,50 +85,44 @@ async function generateIcsFile(events: any[]) {
         'X-WR-CALDESC:Calendar with all matches of KCorp',
         'TIMEZONE-ID:UTC',
         'X-WR-TIMEZONE:UTC',
-    ]
+    ];
     value = value.replace('PRODID:adamgibbons/ics', calendarSettings.join('\n'));
-    fs.writeFile(FTP_PATH, value, 'utf8', (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    });
-    console.log('ICS file created');
-    if (value) await uploadToFTP(value);
+    await fs.writeFile(FTP_PATH, value, 'utf8');
+    return 'ICS file created';
 }
 
-async function uploadToFTP(content: string) {
+async function uploadToFTP(): Promise<string> {
     const client = new ftp.Client();
     client.ftp.verbose = false;
     try {
         await client.access({
             host: FTP_HOST,
+            port: parseInt(FTP_PORT),
             user: FTP_USER,
             password: FTP_PASS,
-            // secure: true,
         });
         await client.uploadFrom(FTP_PATH, `/www/${FTP_PATH}`);
-        console.log('ICS file uploaded to FTP');
     } catch (err) {
         console.error(err);
     }
     client.close();
+    return 'ICS file uploaded to FTP';
 }
 
-async function main() {
-    await getTeams('Karmine');
-
+async function main(teamName: string): Promise<void> {
+    const teams: Team[] = await getTeams(teamName);
     const currentDate = new Date();
     const startDate = `${currentDate.getFullYear()}-01-01`;
     const endDate = `${currentDate.getFullYear()}-12-31`;
 
+    const matches: Match[] = [];
     for (const team of teams) {
-        await getUpcomingMatches(team.id);
-        await getPastMatches(team.id, startDate, endDate);
+        getUpcomingMatches(team).then(data => matches.push(...data));
+        await getPastMatches(team, startDate, endDate).then(data => matches.push(...data));
     }
 
-    const icsEvents = [...upcomingMatches, ...pastMatches].map(createIcsEvent);
-    await generateIcsFile(icsEvents);
+    console.log(await generateIcsFile(matches.map(createIcsEvent)));
+    console.log(await uploadToFTP());
 }
 
-main();
+main('Karmine');
---
+++ src/models/Event.ts
@@ -0,0 +1,11 @@
+export default class Event {
+    uid: string;
+    sequence: number;
+    created: number;
+    lastModified: number;
+    start: number;
+    end: number;
+    title: string;
+    url: string;
+    location: string;
+}

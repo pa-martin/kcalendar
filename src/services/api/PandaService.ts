d88ff2a26f3132b030aeb896f981126e9937f8c2
PAM <pa.martin@outlook.fr>
2025-11-26 17:01:58 +0100

    Fix teams not iterable

--- src/services/api/PandaService.ts
+++ src/services/api/PandaService.ts
@@ -25,41 +25,31 @@ export class PandaService {
     private matches: Record<number, Match[]> = {};
 
     async getTeams(teamName: string): Promise<Team[]> {
-        for (let i = 1; i <= 5; i++) {
-            this.teams = await fetch(`${BASE_URL}/teams?search[name]=${teamName}`, options)
-                .then(response => response.json())
-                .then(data => data)
-                .catch(err => console.error((err as TypeError).message));
-            if (this.teams?.length){
-                console.log(`Found ${this.teams.length} teams after ${i} attempts.`);
-                // On sort de la boucle si on a trouvé des équipes
-                break;
-            } else if (i < 5) {
-                // sleep 5 seconds
-                await new Promise(resolve => setTimeout(resolve, 5000));
-            } else {
-                console.log('No teams found');
-            }
+        await fetch(`${BASE_URL}/teams?search[name]=${teamName}`, options)
+            .then(response => response.json())
+            .then(data => data?.error ? [] : data)
+            .then(data => this.teams = data)
+            .catch(err => console.error((err as TypeError).message));
+        
+        if (this.teams?.length) {
+            console.log(`Found ${this.teams.length} teams.`);
+        } else {
+            console.log('No teams found');
         }
         return this.teams;
     }
 
     private async fetchMatches(team: Team): Promise<void> {
-        for (let i = 1; i <= 5; i++) {
-            this.matches[team.id] = await fetch(`${BASE_URL}/matches?filter[opponent_id]=${team.id}&range[scheduled_at]=${startDate},${endDate}`, options)
-                .then(response => response.json())
-                .then(data => data)
-                .catch(err => console.error((err as TypeError).message));
-            if (this.matches[team.id]?.length){
-                console.log(`Found ${this.matches[team.id]?.length} matches for team ${team.slug} after ${i} attempts.`);
-                // On sort de la boucle si on a trouvé des matchs
-                break;
-            } else if (i < 5) {
-                // sleep 5 seconds
-                await new Promise(resolve => setTimeout(resolve, 5000));
-            } else {
-                console.log(`No matches found for team ${team.slug}`);
-            }
+        await fetch(`${BASE_URL}/matches?filter[opponent_id]=${team.id}&range[scheduled_at]=${startDate},${endDate}`, options)
+            .then(response => response.json())
+            .then(data => data?.error ? [] : data)
+            .then(data => this.matches[team.id] = data)
+            .catch(err => console.error((err as TypeError).message));
+
+        if (this.matches[team.id]?.length) {
+            console.log(`Found ${this.matches[team.id]?.length} matches for team ${team.slug}.`);
+        } else {
+            console.log(`No matches found for team ${team.slug}`);
         }
     }
 
